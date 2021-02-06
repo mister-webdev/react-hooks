da365ebaeabf3fd82dcccbb30e5238f71f06272d
Mister-webdev <roman.pogo@yandex.ru>
2021-02-07 02:02:23 +0600

    new hooks

---
+++ useOnScreen.js
@@ -0,0 +1,59 @@
+/* 
+This hook allows you to easily detect when an element is visible 
+on the screen as well as specify how much of the element should 
+be visible before being considered on screen. Perfect for 
+lazy loading images or triggering animations when the user 
+has scrolled down to a particular section.
+*/
+
+const useOnScreen = (ref, rootMargin = '0px') => {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIntersecting(entry.isIntersecting);
+      },
+      {
+        rootMargin,
+      }
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      observer.unobserve(ref.current);
+    };
+  }, []);
+  return isIntersecting;
+};
+
+// Example
+const App = () => {
+  const ref = useRef();
+  const onScreen = useOnScreen(ref, '-300px');
+
+  return (
+    <div>
+      <div style={{ height: '100vh' }}>
+        <h1>Scroll down to next section 👇</h1>
+      </div>
+      <div
+        ref={ref}
+        style={{
+          height: '100vh',
+          backgroundColor: onScreen ? '#23cebd' : '#efefef',
+        }}
+      >
+        {onScreen ? (
+          <div>
+            <h1>Hey I'm on the screen</h1>
+            <img src="https://i.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif" />
+          </div>
+        ) : (
+          <h1>Scroll down 300px from the top of this section 👇</h1>
+        )}
+      </div>
+    </div>
+  );
+};
